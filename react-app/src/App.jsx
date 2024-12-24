4764f520d48b272da324172d4339b3d11ff6fd4b
Jeff <j3ffh95@gmail.com>
2024-12-23 23:27:24 -0500

    Testing React Conditionals

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,11 +3,15 @@ import Chicken from "./Chicken.jsx";
 import Greeter from "./Greeter.jsx";
 import Dice from "./Dice.jsx";
 import ListPicker from "../ListPicker.jsx";
+import DoubleDice from "../DoubleDice.jsx";
 
 function App() {
   return (
     <div>
-      <ListPicker values={[1, 2, 3]} />
+      <DoubleDice />
+      <DoubleDice />
+      <DoubleDice />
+      {/* <ListPicker values={[1, 2, 3]} /> */}
       {/* <Chicken />
       <Greeter person='Jeff' age='29' />
       <Greeter age='29' />
