2253c1e25855f9944b262799ab6c9095fbbbdb65
Jeff <j3ffh95@gmail.com>
2024-12-24 01:08:36 -0500

    Using the react developer tools

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -11,14 +11,14 @@ function App() {
       <DoubleDice />
       <DoubleDice />
       <DoubleDice />
-      {/* <ListPicker values={[1, 2, 3]} /> */}
-      {/* <Chicken />
+      <ListPicker values={[1, 2, 3]} />
+      <Chicken />
       <Greeter person='Jeff' age='29' />
       <Greeter age='29' />
       <Greeter person='Rosa' age='29' />
       <Dice numSides={20} />
       <Dice />
-      <Dice numSides={10} /> */}
+      <Dice numSides={10} />
     </div>
   );
 }
