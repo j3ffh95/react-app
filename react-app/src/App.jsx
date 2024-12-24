e746594313a66bba7b21dccadcfbf969d188d726
Jeff <j3ffh95@gmail.com>
2024-12-23 23:06:14 -0500

    Passing arrays and objects

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,17 +2,19 @@ import "./App.css";
 import Chicken from "./Chicken.jsx";
 import Greeter from "./Greeter.jsx";
 import Dice from "./Dice.jsx";
+import ListPicker from "../ListPicker.jsx";
 
 function App() {
   return (
     <div>
-      <Chicken />
+      <ListPicker values={[1, 2, 3]} />
+      {/* <Chicken />
       <Greeter person='Jeff' age='29' />
       <Greeter age='29' />
       <Greeter person='Rosa' age='29' />
       <Dice numSides={20} />
       <Dice />
-      <Dice numSides={10} />
+      <Dice numSides={10} /> */}
     </div>
   );
 }
