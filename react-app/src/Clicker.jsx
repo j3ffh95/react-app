76fbdc6638434a100e27f0c275e7813f93c799ba
Jeff <j3ffh95@gmail.com>
2024-12-29 23:30:53 -0500

    Added on hover function

--- react-app/src/Clicker.jsx
+++ react-app/src/Clicker.jsx
@@ -2,11 +2,15 @@ function handleClick() {
   console.log("Clicked the button.");
 }
 
+function handleHover() {
+  console.log("you hover over.");
+}
+
 export default function Clicker() {
   return (
     <div>
-      <p>Click the Button</p>
-      <button onMouseOver={handleClick}>Click</button>
+      <p onMouseOver={handleHover}>Hover over me</p>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
