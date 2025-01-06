e1cb0c06c20ef2faceb0eca5bb4d628be59aed46
Jeff <j3ffh95@gmail.com>
2025-01-06 13:59:16 -0500

    Working with the useState hook

--- react-app/src/Counter.jsx
+++ react-app/src/Counter.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
 export default function Counter() {
+  const [num, setNum] = useState(5);
+  const changeNum = () => {
+    setNum(num + 1);
+  };
   return (
     <div>
-      <p>The count is: </p>
-      <button>Increment</button>
+      <p>The count is: {num}</p>
+      <button onClick={changeNum}>Increment</button>
     </div>
   );
 }
