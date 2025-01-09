63584cd274cdd44beeb4b85148cf152ef788c7a2
Jeff <j3ffh95@gmail.com>
2025-01-08 23:53:10 -0500

    Setting State With An Update

---
+++ state-demo/src/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const incrementByOne = () => setCount(count + 1);
+  const incrementByThree = () => {
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+  };
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={incrementByOne}>+1</button>
+      <button onClick={incrementByThree}>+3</button>
+    </>
+  );
+}
