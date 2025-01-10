bd36044e387d05eb6b6d99163d0beeb5802b8c26
Jeff <j3ffh95@gmail.com>
2025-01-09 22:30:44 -0500

    Made the  useState function without parenthesis so not calling it so it can only render once

--- state-demo/src/Dumbo.jsx
+++ state-demo/src/Dumbo.jsx
@@ -5,7 +5,7 @@ function generateGameBoard() {
   return Array(5000);
 }
 export default function Dumbo() {
-  const [board, setBoard] = useState(generateGameBoard());
+  const [board, setBoard] = useState(generateGameBoard);
   return (
     <button onClick={() => setBoard("Hello")}>Click me to change state</button>
   );
