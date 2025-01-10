c27665ecf88ec493373f24acc019ea2e54731405
Jeff <j3ffh95@gmail.com>
2025-01-09 22:25:47 -0500

    Render the Dumbo component

--- state-demo/src/App.jsx
+++ state-demo/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Counter from "./Counter";
+import Dumbo from "./Dumbo";
 
 function App() {
   return (
     <>
       <h1>State Demo</h1>
-      <Counter />
+      {/* <Counter /> */}
+      <Dumbo />
     </>
   );
 }
