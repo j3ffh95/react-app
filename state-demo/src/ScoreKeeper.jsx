cdabfdf0af0edf20a4d04a9018a8bf8ea8b2d47b
Jeff <j3ffh95@gmail.com>
2025-01-13 17:31:42 -0500

    Made an increase player one function

--- state-demo/src/ScoreKeeper.jsx
+++ state-demo/src/ScoreKeeper.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export { useState } from "react";
 export default function ScoreKeeper() {
+  const [scores, setScores] = useState({ p1Score: 0, p2Score: 0 });
+
+  const increaseP1Score = () => {
+    scores.p1Score += 1;
+    console.log(scores);
+    setScores(scores);
+  };
   return (
     <div>
-      <p>Player 1:</p>
-      <p>Player 2:</p>
-      <button>+1 Player 1</button>
+      <p>Player 1: {scores.p1Score}</p>
+      <p>Player 2: {scores.p2Score}</p>
+      <button onClick={increaseP1Score}>+1 Player 1</button>
       <button>+1 Player 2</button>
     </div>
   );
