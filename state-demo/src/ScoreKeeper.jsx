b888c2e51004e84b52238edde97f379a71cf920b
Jeff <j3ffh95@gmail.com>
2025-01-13 17:22:45 -0500

    Created the Scorekeeper componenet

---
+++ state-demo/src/ScoreKeeper.jsx
@@ -0,0 +1,11 @@
+export { useState } from "react";
+export default function ScoreKeeper() {
+  return (
+    <div>
+      <p>Player 1:</p>
+      <p>Player 2:</p>
+      <button>+1 Player 1</button>
+      <button>+1 Player 2</button>
+    </div>
+  );
+}
