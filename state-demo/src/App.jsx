371023b7199bed06dfa8210cc437d31ffad377a9
Jeff <j3ffh95@gmail.com>
2025-01-13 17:23:41 -0500

    Render Scorekeeper component

--- state-demo/src/App.jsx
+++ state-demo/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Counter from "./Counter";
 import Dumbo from "./Dumbo";
+import ScoreKeeper from "./ScoreKeeper";
 
 function App() {
   return (
     <>
       <h1>State Demo</h1>
-      <Counter />
+      <ScoreKeeper />
+      {/* <Counter /> */}
       {/* <Dumbo /> */}
     </>
   );
