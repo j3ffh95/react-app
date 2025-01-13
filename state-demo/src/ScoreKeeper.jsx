8f3a452006612aea89fa4db4326a628ca0243268
Jeff <j3ffh95@gmail.com>
2025-01-13 17:42:59 -0500

    Added new increase function for player  2

--- state-demo/src/ScoreKeeper.jsx
+++ state-demo/src/ScoreKeeper.jsx
@@ -10,12 +10,18 @@ export default function ScoreKeeper() {
       return { ...oldScores, p1Score: oldScores.p1Score + 1 };
     });
   };
+
+  const increaseP2Score = () => {
+    setScores(oldScores => {
+      return { ...oldScores, p2Score: oldScores.p2Score + 1 };
+    });
+  };
   return (
     <div>
       <p>Player 1: {scores.p1Score}</p>
       <p>Player 2: {scores.p2Score}</p>
       <button onClick={increaseP1Score}>+1 Player 1</button>
-      <button>+1 Player 2</button>
+      <button onClick={increaseP2Score}>+1 Player 2</button>
     </div>
   );
 }
