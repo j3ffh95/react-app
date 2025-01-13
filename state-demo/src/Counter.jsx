e5f9ac48719a0262291d0fecc28db437037f2f33
Jeff <j3ffh95@gmail.com>
2025-01-13 17:17:57 -0500

    Made a button to set the state to ten

--- state-demo/src/Counter.jsx
+++ state-demo/src/Counter.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 
 export default function Counter() {
+  console.log("RENDERED!!!");
   const [count, setCount] = useState(0);
   const incrementByOne = () => setCount(count + 1);
+
+  const setToTen = () => {
+    setCount(10);
+  };
   const incrementByThree = () => {
     setCount(prevCount => prevCount + 1);
     setCount(prevCount => prevCount + 1);
@@ -22,6 +27,7 @@ export default function Counter() {
       <button onClick={incrementByOne}>+1</button>
       <button onClick={incrementByThree}>+3</button>
       <button onClick={incrementByFive}>+5</button>
+      <button onClick={setToTen}>Set to 10</button>
     </>
   );
 }
