9fa7be4c1673aa0602bc7eaac44df88d236af12b
Jeff <j3ffh95@gmail.com>
2025-01-29 20:57:27 -0500

    Created a shopping cart array with objects

---
+++ state-demo/src/commonArrayPatterns.js
@@ -0,0 +1,6 @@
+// COMMON PATTERNS FOR UPDATING ARRAYS IN STATE
+const shoppingCart = [
+  { id: 1, product: "HDMI Cable", price: 4 },
+  { id: 2, product: "Easy Bake Oven", price: 28 },
+  { id: 3, product: "Peach Pie", price: 6.5 },
+];
