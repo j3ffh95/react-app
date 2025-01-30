ec745a7a98e8c5d7c7c1d654345a5a321e79ca1f
Jeff <j3ffh95@gmail.com>
2025-01-29 20:58:44 -0500

    Adding to an array using the spread operator

--- state-demo/src/commonArrayPatterns.js
+++ state-demo/src/commonArrayPatterns.js
@@ -4,3 +4,6 @@ const shoppingCart = [
   { id: 2, product: "Easy Bake Oven", price: 28 },
   { id: 3, product: "Peach Pie", price: 6.5 },
 ];
+
+// ADDING TO AN ARRAY
+[...shoppingCart, { id: 4, product: "Coffee Mug", price: 7.99 }];
