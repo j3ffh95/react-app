6640c95f084074e5c13c1129844fbaca1e627c28
Jeff <j3ffh95@gmail.com>
2025-02-19 23:34:37 -0500

    Create incrementScore function

--- state-demo/src/ScoreKeeper.jsx
+++ state-demo/src/ScoreKeeper.jsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 
 export default function ScoreKeeper({ numPlayers = 3, target = 5 }) {
   const [scores, setScores] = useState(new Array(numPlayers).fill(0));
+  const incrementScore = index => {
+    setScores(prevScores => {
+      const copy = [...prevScores];
+    });
+  };
 
   return (
     <div>
       <h1>Score Keeper</h1>
-      <ul></ul>
+      <ul>
+        {scores.map((score, index) => {
+          return (
+            <li key={index}>
+              Player {index + 1}: {score}
+              <button>+1</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
