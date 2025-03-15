e421a74d5a763028e303a7f89d3317e11263cf98
Jeff <j3ffh95@gmail.com>
2025-03-14 22:12:03 -0400

    Created the title prop for each different game

--- state-demo/src/App.jsx
+++ state-demo/src/App.jsx
@@ -10,12 +10,21 @@ import Lucky7 from "./Lucky7";
 import Die from "./Die";
 import Dice from "./Dice";
 import LuckyN from "./LuckyN";
+import { sum } from "./utils";
+
+function lessThan4(dice) {
+  return sum(dice) < 4;
+}
+
+function sameNums(dice) {
+  return dice.every(v => v === dice[0]);
+}
 
 function App() {
   return (
     <>
-      <LuckyN />
-      <LuckyN numDice={3} goal={11} />
+      <LuckyN title='Roll less than 4!' winCheck={lessThan4} />
+      <LuckyN title='Roll the same numbers!' winCheck={sameNums} />
       {/* <Dice dice={[1, 2, 3]} />
       <Dice dice={[1, 2, 3, 5, 2, 5, 8]} color='green' />
       <Dice dice={[1, 2, 3]} /> */}
--- state-demo/src/LuckyN.jsx
+++ state-demo/src/LuckyN.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { getRolls } from "./utils";
 import Dice from "./Dice";
-function LuckyN({ numDice = 2, winCheck }) {
+function LuckyN({ numDice = 2, winCheck, title }) {
   const [dice, setDice] = useState(getRolls(numDice));
   const isWinner = winCheck(dice);
   const roll = () => setDice(getRolls(numDice));
   return (
     <main className='LuckyN'>
-      <h1>Lucky Game: {isWinner && "You Win!"}</h1>
+      <h1>
+        {title}: {isWinner && "You Win!"}
+      </h1>
       <Dice dice={dice} />
       <button onClick={roll}>Re-Roll Dice</button>
     </main>
