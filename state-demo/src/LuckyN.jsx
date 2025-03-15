6c8e207bd15b89b2a00926f8e5804b0a44f44f16
Jeff <j3ffh95@gmail.com>
2025-03-14 21:37:58 -0400

    Returned the main tag to render the component

--- state-demo/src/LuckyN.jsx
+++ state-demo/src/LuckyN.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import { getRolls } from "./utils";
 function LuckyN({ numDice = 2, goal = 7 }) {
   const [dice, setDice] = useState(getRolls(numDice));
+  return (
+    <main className='LuckyN'>
+      <h1>Lucky{goal}</h1>
+    </main>
+  );
 }
 
 export default LuckyN;
