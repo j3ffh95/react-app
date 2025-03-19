280ab065425f6ddf44c0f685e956c05b653691a7
Jeff <j3ffh95@gmail.com>
2025-03-18 20:11:39 -0400

    Created a username form component

---
+++ react-forms/src/UsernameForm.jsx
@@ -0,0 +1,10 @@
+function UsernameForm() {
+  return (
+    <div>
+      <input type='text' placeholder='Username' />
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default UsernameForm;
