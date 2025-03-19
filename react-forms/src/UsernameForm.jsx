af2a3ea0f173637228166e25334713589b62309b
Jeff <j3ffh95@gmail.com>
2025-03-18 20:13:54 -0400

    Imported the usestate to look for the username

--- react-forms/src/UsernameForm.jsx
+++ react-forms/src/UsernameForm.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+
 function UsernameForm() {
+  const [username, setUsername] = useState("");
   return (
     <div>
-      <input type='text' placeholder='Username' />
+      <input type='text' placeholder='Username' value={username} />
       <button>Submit</button>
     </div>
   );
