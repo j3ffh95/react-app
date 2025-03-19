be0e21b1c7bdd171f0208738550fc03d09f92b65
Jeff <j3ffh95@gmail.com>
2025-03-18 20:36:12 -0400

    Controlled components

--- react-forms/src/UsernameForm.jsx
+++ react-forms/src/UsernameForm.jsx
@@ -2,9 +2,18 @@ import { useState } from "react";
 
 function UsernameForm() {
   const [username, setUsername] = useState("Jefferson");
+  const updateUsername = event => {
+    // console.log(event.target.value);
+    setUsername(event.target.value);
+  };
   return (
     <div>
-      <input type='text' placeholder='Username' value={username} />
+      <input
+        type='text'
+        placeholder='Username'
+        value={username}
+        onChange={updateUsername}
+      />
       <button>Submit</button>
     </div>
   );
