74ea210f4e425b0f15b265bb3f833501bf78e716
Jeff <j3ffh95@gmail.com>
2025-04-17 20:59:17 -0400

    Created new component called BetterSignUpForm

---
+++ react-forms/src/BetterSignUpForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function BetterSignUpForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const updateFirstName = event => {
+    // console.log(event.target.value);
+    setFirstName(event.target.value);
+  };
+  const updateLastName = event => {
+    // console.log(event.target.value);
+    setLastName(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(firstName, lastName);
+  };
+  return (
+    <div>
+      <label htmlFor='firstName'>Enter a first name: </label>
+      <input
+        type='text'
+        placeholder='First Name'
+        value={firstName}
+        onChange={updateFirstName}
+        id='firstName'
+      />
+      <label htmlFor='lastName'>Enter a last name: </label>
+      <input
+        type='text'
+        placeholder='Last Name'
+        value={lastName}
+        onChange={updateLastName}
+        id='lastName'
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default BetterSignUpForm;
